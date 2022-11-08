2d4bc0be038a5ad60853bd9ece064db317669034
jdiaz97 <jose.diaz1601@alumnos.ubiobio.cl>
2022-11-08 16:27:40 +0100

    update

--- core.jl
+++ core.jl
@@ -1,4 +1,5 @@
 include("resources.jl")
+include("token.jl")
 
 ## Main Functions
 """
@@ -25,4 +26,28 @@ function regions()
     return toquery(query)
 end
 
-species_by_country()
+"""
+Species conservation measures information, either by species ID or species name \n 
+Please refer to the Measures classification scheme to learn more.
+"""
+function conservation_measure(name::String)
+    query = "/api/v3/measures/species/name/"*name*"?token="*token
+    return toquery(query)
+end
+
+function conservation_measure(name::String,region::String)
+    query = "/api/v3/measures/species/name/"*name*"/region/"*region*"?token="*token
+    return toquery(query)
+end
+
+function conservation_measure(id::Int64)
+    id = String(id)
+    query = "/api/v3/measures/species/id/"*id*"?token="*token
+    return toquery(query)
+end
+
+function conservation_measure(id::Int64,region::String)
+    id = String(id)
+    query = "/api/v3/measures/species/id/"*name*"/region/"*region*"?token="*token
+    return toquery(query)
+end
--- resources.jl
+++ resources.jl
@@ -1,9 +1,11 @@
 using HTTP
 using JSON
 
-## Resources
 function toquery(url)
     a = HTTP.get("https://apiv3.iucnredlist.org"*url)
     return JSON.parse(String(a.body))
 end
 
+## melt_dict
+
+## iucn_df
